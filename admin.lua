41e7a1e44a9470f47354f0858db4bfe8b5159348
stuudmuffin <taytrho@gmail.com>
2016-10-24 23:45:00 -0600

    Added Character Stat Modification GUI for Admins (#2)
    * Moved some things around
    
    Just moved some things for order.
    
    * Added Character Stat Modification GUI
    
    Very long commit description here:
    
    The GUI contains four buttons, as well as a choice of five
    radio-buttons.
    -The first button is Pickup. When activated, the admit is able to pickup
    every item on screen at max zoom by pressing F. (WARNING - This includes
    items on transport belts, so be very careful with this)
    -The second button is Reach. This extends the player's reach for all
    aspects: building, item dropping, structure interaction, and ore/tree
    mining. Same as above, will be the entire screen at max zoom level
    -The third button is Crafting. This will increase the crafting speed to
    be 60 times faster, meaning most items are crafted instantly.
    -The fourth button is Mining. This makes mining ores, chopping trees,
    and deconstructing buildings. The speed increase makes it near instant.
    The five radio buttons are only active one at a time. They control the
    run speed. 1x is default run speed, 2x is twice as fast. There's also
    3x, 5x, and 10x.
    
    The update also activates cheat_mode for admins who enter the god
    character controller mode (Spectate mode). This allows crafting to be
    instant and not cost resources.
    Character settings are saved when the admin enters Spectate mode, and
    will be restored to the new character when exiting Spectate mode.
    
    * Fixed Bugs
    
    Missed quite a few bugs because I didn't test before committing...
    Whoops.
    
    * Disabled Button if in Spectate Mode
    
    Meant to do this earlier, never actually put the code in.
    Bugs occur if the admin is in Spectate mode while attempting to modify
    character. Disabled the button so that it couldn't be activated while in
    Spectate mode.
    
    * Fixed Typo
    
    This is the dangers of copy/pasting
    
    * Final Minor Fixes
    
    Spacing, an extra Label element on the GUI

--- admin.lua
+++ admin.lua
@@ -1,34 +1,252 @@
 -- @module admin
 -- A 3Ra Gaming creation
 
--- Handle spectate gui click
+global.green = {r= 0/256, g=  256/256, b=  0/256}
+global.red = {r= 256/256, g=  0/256, b=  0/256}
+
+-- Handle various gui clicks, either spectate or character modification
 -- @param event gui click event
 local function gui_click(event)
     local i = event.player_index
     local p = game.players[i]
     local e = event.element.name
+	if e == "character" and event.element.caption == "Disabled" then
+		p.print("Character modification disabled in Spectator mode.")
+		return
+	end
     if p.gui.top.spectate ~= nil then
         if e ~= nil then
             if e == "spectate" then
                 if not p.admin then
-                    p.gui.left.spectate.destroy()
+                    p.gui.top.spectate.destroy()
                     p.print("You are no longer an admin.")
                     return
                 end
                 force_spectators(i)
+			elseif e == "character" then
+				p.gui.top.character.destroy()
+				create_character_gui(i)
+			elseif e == "character_close" then
+				p.gui.top.character_panel.destroy()
+				p.gui.top.add{name = "character", type = "button", direction = "vertical", caption = "Character"}
+			elseif e == "character_pickup" then
+				if global.player_character_stats[i].item_loot_pickup then
+					global.player_character_stats[i].item_loot_pickup = false
+					event.element.style.font_color = global.red
+					p.character_item_pickup_distance_bonus = 0
+					p.character_loot_pickup_distance_bonus = 0
+				else
+					global.player_character_stats[i].item_loot_pickup = true
+					event.element.style.font_color = global.green
+					p.character_item_pickup_distance_bonus = 125
+					p.character_loot_pickup_distance_bonus = 125
+				end
+			elseif e == "character_reach" then
+				if global.player_character_stats[i].build_itemdrop_reach_resourcereach_distance then
+					global.player_character_stats[i].build_itemdrop_reach_resourcereach_distance = false
+					event.element.style.font_color = global.red
+					p.character_build_distance_bonus = 0
+					p.character_item_drop_distance_bonus = 0
+					p.character_reach_distance_bonus = 0
+					p.character_resource_reach_distance_bonus = 0
+				else
+					global.player_character_stats[i].build_itemdrop_reach_resourcereach_distance = true
+					event.element.style.font_color = global.green
+					p.character_build_distance_bonus = 125
+					p.character_item_drop_distance_bonus = 125
+					p.character_reach_distance_bonus = 125
+					p.character_resource_reach_distance_bonus = 125
+				end
+			elseif e == "character_craft" then
+				if global.player_character_stats[i].crafting_speed then
+					global.player_character_stats[i].crafting_speed = false
+					event.element.style.font_color = global.red
+					p.character_crafting_speed_modifier = 0
+				else
+					global.player_character_stats[i].crafting_speed = true
+					event.element.style.font_color = global.green
+					p.character_crafting_speed_modifier = 60
+				end
+			elseif e == "character_mine" then
+				if global.player_character_stats[i].mining_speed then
+					global.player_character_stats[i].mining_speed = false
+					event.element.style.font_color = global.red
+					p.character_mining_speed_modifier = 0
+				else
+					global.player_character_stats[i].mining_speed = true
+					event.element.style.font_color = global.green
+					p.character_mining_speed_modifier = 150
+				end
+			elseif e == "character_run1" then
+				local run_table = event.element.parent
+				run_table.character_run1.state = true
+				run_table.character_run2.state = false
+				run_table.character_run3.state = false
+				run_table.character_run5.state = false
+				run_table.character_run10.state = false
+				p.character_running_speed_modifier = 0
+				global.player_character_stats[i].running_speed = 0
+			elseif e == "character_run2" then
+				local run_table = event.element.parent
+				run_table.character_run1.state = false
+				run_table.character_run2.state = true
+				run_table.character_run3.state = false
+				run_table.character_run5.state = false
+				run_table.character_run10.state = false
+				p.character_running_speed_modifier = 1
+				global.player_character_stats[i].running_speed = 1
+			elseif e == "character_run3" then
+				local run_table = event.element.parent
+				run_table.character_run1.state = false
+				run_table.character_run2.state = false
+				run_table.character_run3.state = true
+				run_table.character_run5.state = false
+				run_table.character_run10.state = false
+				p.character_running_speed_modifier = 2
+				global.player_character_stats[i].running_speed = 2
+			elseif e == "character_run5" then
+				local run_table = event.element.parent
+				run_table.character_run1.state = false
+				run_table.character_run2.state = false
+				run_table.character_run3.state = false
+				run_table.character_run5.state = true
+				run_table.character_run10.state = false
+				p.character_running_speed_modifier = 4
+				global.player_character_stats[i].running_speed = 4
+			elseif e == "character_run10" then
+				local run_table = event.element.parent
+				run_table.character_run1.state = false
+				run_table.character_run2.state = false
+				run_table.character_run3.state = false
+				run_table.character_run5.state = false
+				run_table.character_run10.state = true
+				p.character_running_speed_modifier = 9
+				global.player_character_stats[i].running_speed = 9
             end
         end
     end
 end
 
+-- Create the full character GUI for admins to update their character settings
+-- @param index index of the player to change
+function create_character_gui(index)
+	local player = game.players[index]
+	local character_frame = player.gui.top.add{name = "character_panel", type = "frame", direction = "vertical", caption = "Character"}
+	character_frame.add{name = "character_pickup", type = "button", caption = "Pickup"}
+	character_frame.add{name = "character_reach", type = "button", caption = "Reach"}
+	character_frame.add{name = "character_craft", type = "button", caption = "Crafting"}
+	character_frame.add{name = "character_mine", type = "button", caption = "Mining"}
+	character_frame.add{name = "run_label", type = "label", caption = "Run speed control:"}
+	local run_table = character_frame.add{name = "character_run", type = "table", colspan = 5, caption = "Run Speed"}
+	run_table.add{name = "run1_label", type = "label", caption = "1x"}
+	run_table.add{name = "run2_label", type = "label", caption = "2x"}
+	run_table.add{name = "run3_label", type = "label", caption = "3x"}
+	run_table.add{name = "run5_label", type = "label", caption = "5x"}
+	run_table.add{name = "run10_label", type = "label", caption = "10x"}
+	run_table.add{name = "character_run1", type = "radiobutton", state = false}
+	run_table.add{name = "character_run2", type = "radiobutton", state = false}
+	run_table.add{name = "character_run3", type = "radiobutton", state = false}
+	run_table.add{name = "character_run5", type = "radiobutton", state = false}
+	run_table.add{name = "character_run10", type = "radiobutton", state = false}
+	character_frame.add{name = "character_close", type = "button", caption = "Close"}
+	update_character_settings(index)
+end
+
+-- Updates the full character GUI to show the current settings
+-- @param index index of the player to change
+function update_character_settings(index)
+	local char_gui = game.players[index].gui.top.character_panel
+	local settings = global.player_character_stats[index]
+	
+	if settings.item_loot_pickup then
+		char_gui.character_pickup.style.font_color = global.green
+	else
+		char_gui.character_pickup.style.font_color = global.red
+	end
+	
+	if settings.build_itemdrop_reach_resourcereach_distance then
+		char_gui.character_reach.style.font_color = global.green
+	else
+		char_gui.character_reach.style.font_color = global.red
+	end
+	
+	if settings.crafting_speed then
+		char_gui.character_craft.style.font_color = global.green
+	else
+		char_gui.character_craft.style.font_color = global.red
+	end
+	
+	if settings.mining_speed then
+		char_gui.character_mine.style.font_color = global.green
+	else
+		char_gui.character_mine.style.font_color = global.red
+	end
+	
+	local run_table = char_gui.character_run
+	if settings.running_speed == 0 then
+		run_table.character_run1.state = true
+	elseif settings.running_speed == 1 then
+		run_table.character_run2.state = true
+	elseif settings.running_speed == 2 then
+		run_table.character_run3.state = true
+	elseif settings.running_speed == 4 then
+		run_table.character_run5.state = true
+	elseif settings.running_speed == 9 then
+		run_table.character_run10.state = true
+	end
+end
+
+-- Updates the new character of an admin coming out of spectate mode
+-- @param index index of the player to change
+function update_character(index)
+	local player = game.players[index]
+	local settings = global.player_character_stats[index]
+	
+	if settings.item_loot_pickup then
+		player.character_item_pickup_distance_bonus = 125
+		player.character_loot_pickup_distance_bonus = 125
+	end
+	
+	if settings.build_itemdrop_reach_resourcereach_distance then
+		player.character_item_drop_distance_bonus = 125
+		player.character_reach_distance_bonus = 125
+		player.character_resource_reach_distance_bonus = 125
+	end
+	
+	if settings.crafting_speed then
+		player.character_crafting_speed_modifier = 60
+	end
+	
+	if settings.mining_speed then
+		player.character_mining_speed_modifier = 150
+	end
+	
+	player.character_running_speed_modifier = settings.running_speed
+end
+
 -- Announce an admin's joining and give them the admin gui
 -- @param event player joined event
 local function admin_joined(event)
-    local player = game.players[event.player_index]
+	local index = event.player_index
+    local player = game.players[index]
+	global.player_character_stats = global.player_character_stats or {}
     if player.admin then
         if not player.gui.top.spectate then
-            local adminframe = player.gui.top.add{name = "spectate", type = "button", direction = "horizontal", caption = "Spectate"}
+            player.gui.top.add{name = "spectate", type = "button", direction = "horizontal", caption = "Spectate"}
         end
+		if not player.gui.top.character then
+			player.gui.top.add{name = "character", type = "button", direction = "vertical", caption = "Character"}
+		end
+		if global.player_character_stats[index] == nil then
+			global.player_character_stats[index] = {
+				item_loot_pickup = false,
+				build_itemdrop_reach_resourcereach_distance = false,
+				crafting_speed = false,
+				mining_speed = false,
+				running_speed = 0
+			}
+		end
+			
         game.print("All Hail Admin "..player.name)
     end
 
@@ -56,6 +274,8 @@ function force_spectators(index)
         player.force = game.forces[global.player_spectator_force[index].name]
         player.print("Summoning your character")
         player.gui.top.spectate.caption = "Spectate"
+		player.gui.top.character.caption = "Character"
+		update_character(index)
     else
         --put player in spectator mode
         if player.character then
@@ -63,12 +283,18 @@ function force_spectators(index)
             global.player_spectator_character[index] = player.character
             global.player_spectator_force[index] = player.force
     		player.set_controller{type = defines.controllers.god}
+			player.cheat_mode = true
         end
         if not game.forces["Spectators"] then game.create_force("Spectators") end
 		player.force = game.forces["Spectators"]
         global.player_spectator_state[index] = true
 		player.print("You are now a spectator")
         player.gui.top.spectate.caption = "Return"
+		if player.gui.top.character_panel ~= nil then
+			player.gui.top.character_panel.destroy()
+			player.gui.top.add{name = "character", type = "button", direction = "vertical", caption = "Character"}
+		end
+		player.gui.top.character.caption = "Disabled"
     end
 end
 
