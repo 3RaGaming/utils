170dc80de2fdf7eb9ba7a4f81f6a60e4eb03f74f
Artem Vasenin <vasart169@gmail.com>
2016-11-01 21:11:40 +0000

    Change formatting to official Lua style guide

--- event.lua
+++ event.lua
@@ -3,39 +3,39 @@
 -- @module Event
 
 function fail_if_missing(var, msg)
-    if not var then
-        if msg then
-            error(msg, 3)
-        else
-            error("Missing value", 3)
-        end
+  if not var then
+    if msg then
+      error(msg, 3)
+    else
+      error("Missing value", 3)
     end
-    return false
+  end
+  return false
 end
 
 
 Event = {
-    _registry = {},
-    core_events = {
-        init = -1,
-        load = -2,
-        configuration_changed = -3,
-        _register = function(id)
-            if id == Event.core_events.init then
-                script.on_init(function()
-                    Event.dispatch({name = Event.core_events.init, tick = game.tick})
-                end)
-            elseif id == Event.core_events.load then
-                script.on_load(function()
-                    Event.dispatch({name = Event.core_events.load, tick = -1})
-                end)
-            elseif id == Event.core_events.configuration_changed then
-                script.on_configuration_changed(function(data)
-                    Event.dispatch({name = Event.core_events.configuration_changed, tick = game.tick, data = data})
-                end)
-            end
-        end
-    }
+  _registry = {},
+  core_events = {
+    init = -1,
+    load = -2,
+    configuration_changed = -3,
+    _register = function(id)
+      if id == Event.core_events.init then
+        script.on_init(function()
+          Event.dispatch({ name = Event.core_events.init, tick = game.tick })
+        end)
+      elseif id == Event.core_events.load then
+        script.on_load(function()
+          Event.dispatch({ name = Event.core_events.load, tick = -1 })
+        end)
+      elseif id == Event.core_events.configuration_changed then
+        script.on_configuration_changed(function(data)
+          Event.dispatch({ name = Event.core_events.configuration_changed, tick = game.tick, data = data })
+        end)
+      end
+    end
+  }
 }
 
 --- Registers a function for a given event
@@ -43,52 +43,52 @@ Event = {
 -- @param handler Function to call when event is triggered
 -- @return #Event
 function Event.register(event, handler)
-    fail_if_missing(event, "missing event argument")
+  fail_if_missing(event, "missing event argument")
 
-    if type(event) == "number" then
-        event = {event}
-    end
+  if type(event) == "number" then
+    event = { event }
+  end
 
-    for _, event_id in pairs(event) do
-        fail_if_missing(event_id, "missing event id")
-        if handler == nil then
-            Event._registry[event_id] = nil
-            script.on_event(event_id, nil)
+  for _, event_id in pairs(event) do
+    fail_if_missing(event_id, "missing event id")
+    if handler == nil then
+      Event._registry[event_id] = nil
+      script.on_event(event_id, nil)
+    else
+      if not Event._registry[event_id] then
+        Event._registry[event_id] = {}
+
+        if event_id >= 0 then
+          script.on_event(event_id, Event.dispatch)
         else
-            if not Event._registry[event_id] then
-                Event._registry[event_id] = {}
-
-                if event_id >= 0 then
-                    script.on_event(event_id, Event.dispatch)
-                else
-                    Event.core_events._register(event_id)
-                end
-            end
-            table.insert(Event._registry[event_id], handler)
+          Event.core_events._register(event_id)
         end
+      end
+      table.insert(Event._registry[event_id], handler)
     end
-    return Event
+  end
+  return Event
 end
 
 --- Calls the registerd handlers
 -- @param event LuaEvent as created by game.raise_event
 function Event.dispatch(event)
-    fail_if_missing(event, "missing event argument")
-    if Event._registry[event.name] then
-        for _, handler in pairs(Event._registry[event.name]) do
-            local metatbl = { __index = function(tbl, key) if key == '_handler' then return handler else return rawget(tbl, key) end end }
-            setmetatable(event, metatbl)
-            local success, err = pcall(handler, event)
-            if not success then
-                -- may be nil in on_load
-                game.print(err)
-                return
-            end
-            if err then
-                return
-            end
-        end
+  fail_if_missing(event, "missing event argument")
+  if Event._registry[event.name] then
+    for _, handler in pairs(Event._registry[event.name]) do
+      local metatbl = { __index = function(tbl, key) if key == '_handler' then return handler else return rawget(tbl, key) end end }
+      setmetatable(event, metatbl)
+      local success, err = pcall(handler, event)
+      if not success then
+        -- may be nil in on_load
+        game.print(err)
+        return
+      end
+      if err then
+        return
+      end
     end
+  end
 end
 
 --- Removes the handler from the event
@@ -96,28 +96,28 @@ end
 -- @param handler to remove
 -- @return #Event
 function Event.remove(event, handler)
-    fail_if_missing(event, "missing event argument")
-    fail_if_missing(handler, "missing handler argument")
+  fail_if_missing(event, "missing event argument")
+  fail_if_missing(handler, "missing handler argument")
 
-    if type(event) == "number" then
-        event = {event}
-    end
+  if type(event) == "number" then
+    event = { event }
+  end
 
-    for _, event_id in pairs(event) do
-        fail_if_missing(event_id, "missing event id")
-        if Event._registry[event_id] then
-            for i=#Event._registry[event_id], 1, -1 do
-                if Event._registry[event_id][i] == handler then
-                    table.remove(Event._registry[event_id], i)
-                end
-            end
-            if #Event._registry[event_id] == 0 then
-                Event._registry[event_id] = nil
-                script.on_event(event_id, nil)
-            end
+  for _, event_id in pairs(event) do
+    fail_if_missing(event_id, "missing event id")
+    if Event._registry[event_id] then
+      for i = #Event._registry[event_id], 1, -1 do
+        if Event._registry[event_id][i] == handler then
+          table.remove(Event._registry[event_id], i)
         end
+      end
+      if #Event._registry[event_id] == 0 then
+        Event._registry[event_id] = nil
+        script.on_event(event_id, nil)
+      end
     end
-    return Event
+  end
+  return Event
 end
 
 return Event
--- undecorator.lua
+++ undecorator.lua
@@ -6,15 +6,15 @@ local CHUNK_SIZE = 32
 -- Remove decorations in a given area
 -- @param surface target surface
 -- @param area area to clear decorations within
-local function removeDecorationsArea(surface, area )
-    for _, entity in pairs(surface.find_entities_filtered{area = area, type="decorative"}) do
-        entity.destroy()
-    end
+local function removeDecorationsArea(surface, area)
+  for _, entity in pairs(surface.find_entities_filtered { area = area, type = "decorative" }) do
+    entity.destroy()
+  end
 end
 
 --this fires whenever new chunks are generated.
 Event.register(defines.events.on_chunk_generated, function(event)
-    removeDecorationsArea( event.surface, event.area )
+  removeDecorationsArea(event.surface, event.area)
 end)
 --the rest only triggers if they've never been removed before on whichever map is loaded.
 
@@ -24,29 +24,29 @@ end)
 -- @param y bottom left y coordinate
 -- @param width width of area
 -- @param height height of area
-local function removeDecorations(surface, x, y, width, height )
-    removeDecorationsArea(surface, {{x, y}, {x + width, y + height}})
+local function removeDecorations(surface, x, y, width, height)
+  removeDecorationsArea(surface, { { x, y }, { x + width, y + height } })
 end
 
 -- Clear all decorations on the map.
 local function clearDecorations()
-    local surface = game.surfaces["nauvis"]
-    for chunk in surface.get_chunks() do
-        removeDecorations(surface, chunk.x * CHUNK_SIZE, chunk.y * CHUNK_SIZE, CHUNK_SIZE - 1, CHUNK_SIZE - 1)
-    end
-    
-    game.print("Decorations removed")
+  local surface = game.surfaces["nauvis"]
+  for chunk in surface.get_chunks() do
+    removeDecorations(surface, chunk.x * CHUNK_SIZE, chunk.y * CHUNK_SIZE, CHUNK_SIZE - 1, CHUNK_SIZE - 1)
+  end
+
+  game.print("Decorations removed")
 end
 
 -- If the map hasn't been scanned already, clear it
 -- @param event on_tick event
 function full_clear(event)
-    if not global.fullClear then
-        clearDecorations()
-        global.fullClear = true
-    end
-    
-    Event.remove(defines.events.on_tick, full_clear)
+  if not global.fullClear then
+    clearDecorations()
+    global.fullClear = true
+  end
+
+  Event.remove(defines.events.on_tick, full_clear)
 end
 
 Event.register(defines.events.on_tick, full_clear)
