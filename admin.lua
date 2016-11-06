9f47e5606ea351adbd6e130db3323a8394c4e395
Artem Vasenin <vasart169@gmail.com>
2016-11-06 15:29:51 +0000

    Fix follow list duplicate bug

--- admin.lua
+++ admin.lua
@@ -1,8 +1,8 @@
 -- @module admin
 -- A 3Ra Gaming creation
 
-global.green = {r = 0, g = 1, b = 0}
-global.red = {r = 1, g = 0, b = 0}
+global.green = { r = 0, g = 1, b = 0 }
+global.red = { r = 1, g = 0, b = 0 }
 
 global.follow_targets = {}
 global.original_position = {}
@@ -168,8 +168,8 @@ local function gui_click(event)
 				global.original_position[i] = p.position
 				global.original_surface[i] = p.surface
 				global.follow_targets[i] = player.index
-				if not p.gui.left.follow_panel.follow_list.unfollow then p.gui.left.follow_panel.follow_list.add{name = "unfollow", type = "button", caption = "Unfollow"} end
-				if not p.gui.left.follow_panel.follow_list.return_button then p.gui.left.follow_panel.follow_list.add{name = "return_button", type = "button", caption = "Return"} end
+				if not p.gui.left.follow_panel.follow_list.unfollow then p.gui.left.follow_panel.follow_list.add { name = "unfollow", type = "button", caption = "Unfollow" } end
+				if not p.gui.left.follow_panel.follow_list.return_button then p.gui.left.follow_panel.follow_list.add { name = "return_button", type = "button", caption = "Return" } end
 			end
 		end
 	end
@@ -179,23 +179,23 @@ end
 -- @param index index of the player to change
 function create_character_gui(index)
 	local player = game.players[index]
-	local character_frame = player.gui.left.add{name = "character_panel", type = "frame", direction = "vertical", caption = "Character"}
-	character_frame.add{name = "character_pickup", type = "button", caption = "Pickup"}
-	character_frame.add{name = "character_reach", type = "button", caption = "Reach"}
-	character_frame.add{name = "character_craft", type = "button", caption = "Crafting"}
-	character_frame.add{name = "character_mine", type = "button", caption = "Mining"}
-	character_frame.add{name = "run_label", type = "label", caption = "Run speed control:"}
-	local run_table = character_frame.add{name = "character_run", type = "table", colspan = 5, caption = "Run Speed"}
-	run_table.add{name = "run1_label", type = "label", caption = "1x"}
-	run_table.add{name = "run2_label", type = "label", caption = "2x"}
-	run_table.add{name = "run3_label", type = "label", caption = "3x"}
-	run_table.add{name = "run5_label", type = "label", caption = "5x"}
-	run_table.add{name = "run10_label", type = "label", caption = "10x"}
-	run_table.add{name = "character_run1", type = "radiobutton", state = false}
-	run_table.add{name = "character_run2", type = "radiobutton", state = false}
-	run_table.add{name = "character_run3", type = "radiobutton", state = false}
-	run_table.add{name = "character_run5", type = "radiobutton", state = false}
-	run_table.add{name = "character_run10", type = "radiobutton", state = false}
+	local character_frame = player.gui.left.add { name = "character_panel", type = "frame", direction = "vertical", caption = "Character" }
+	character_frame.add { name = "character_pickup", type = "button", caption = "Pickup" }
+	character_frame.add { name = "character_reach", type = "button", caption = "Reach" }
+	character_frame.add { name = "character_craft", type = "button", caption = "Crafting" }
+	character_frame.add { name = "character_mine", type = "button", caption = "Mining" }
+	character_frame.add { name = "run_label", type = "label", caption = "Run speed control:" }
+	local run_table = character_frame.add { name = "character_run", type = "table", colspan = 5, caption = "Run Speed" }
+	run_table.add { name = "run1_label", type = "label", caption = "1x" }
+	run_table.add { name = "run2_label", type = "label", caption = "2x" }
+	run_table.add { name = "run3_label", type = "label", caption = "3x" }
+	run_table.add { name = "run5_label", type = "label", caption = "5x" }
+	run_table.add { name = "run10_label", type = "label", caption = "10x" }
+	run_table.add { name = "character_run1", type = "radiobutton", state = false }
+	run_table.add { name = "character_run2", type = "radiobutton", state = false }
+	run_table.add { name = "character_run3", type = "radiobutton", state = false }
+	run_table.add { name = "character_run5", type = "radiobutton", state = false }
+	run_table.add { name = "character_run10", type = "radiobutton", state = false }
 	update_character_settings(index)
 end
 
@@ -284,14 +284,26 @@ end
 
 local function update_follow_panel(player)
 	local player_index = player.index
+
 	if player.gui.left.follow_panel then
-		local follow_list = player.gui.left.follow_panel.follow_list
+		-- destroy the panel first to make sure we are not duplicating names.
+		if player.gui.left.follow_panel.follow_list then player.gui.left.follow_panel.follow_list.destroy() end
+
+		local follow_list = player.gui.left.follow_panel.add { name = "follow_list", type = "scroll-pane" }
+		-- List of heights:
+		-- Title + 1 button = 45
+		-- Title + 2 buttons = 95
+		-- Title + 3 buttons = 140
+		-- Title + 4 buttons = 190
+		-- The height of a single button is likely not a multiple of 5, which is why these numbers are weird.
+		follow_list.style.maximal_height = 140
+
 		if #game.connected_players == 1 then
-			follow_list.add{name = "no_player_label", type = "label", caption = "There are no players to follow"}
+			follow_list.add { name = "no_player_label", type = "label", caption = "There are no players to follow" }
 		else
-			for _, followplayer in pairs(game.connected_players) do
-				if player.index ~= followplayer.index then
-					follow_list.add{name = followplayer.name, type = "button", caption = followplayer.name}
+			for _, follow_player in pairs(game.connected_players) do
+				if player.index ~= follow_player.index then
+					follow_list.add { name = follow_player.name, type = "button", caption = follow_player.name }
 				end
 			end
 		end
@@ -305,15 +317,7 @@ function toggle_follow_panel(player)
 		global.follow_targets[player.index] = nil
 	else
 		player.gui.left.spectate_panel.follow.caption = "Close"
-		player.gui.left.add{name = "follow_panel", type = "frame", direction = "vertical", caption = "Follow"}
-		local follow_list = player.gui.left.follow_panel.add{name = "follow_list", type = "scroll-pane"}
-		-- List of heights:
-		-- Title + 1 button = 45
-		-- Title + 2 buttons = 95
-		-- Title + 3 buttons = 140
-		-- Title + 4 buttons = 190
-		-- The height of a single button is likely not a multiple of 5, which is why these numbers are weird. 
-		follow_list.style.maximal_height = 140
+		player.gui.left.add { name = "follow_panel", type = "frame", direction = "vertical", caption = "Follow" }
 		update_follow_panel(player)
 	end
 end
@@ -326,7 +330,9 @@ local function connected_players_changed(event)
 	end
 
 	for _, player in pairs(game.connected_players) do
-		update_follow_panel(player)
+		if player.admin then
+			update_follow_panel(player)
+		end
 	end
 end
 
@@ -349,15 +355,15 @@ function create_admin_gui(player_name)
 	local admin_pane = nil
 	global.player_character_stats = global.player_character_stats or {}
 	if not player.gui.left.admin_pane then
-		admin_pane = player.gui.left.add{name = "admin_pane", type = "frame", direction = "vertical", caption = "Admin Tools"}
+		admin_pane = player.gui.left.add { name = "admin_pane", type = "frame", direction = "vertical", caption = "Admin Tools" }
 	else
 		admin_pane = player.gui.left.admin_pane
 	end
 	if not player.gui.left.admin_pane.spectate then
-		admin_pane.add{name = "spectate", type = "button", caption = "Spectate"}
+		admin_pane.add { name = "spectate", type = "button", caption = "Spectate" }
 	end
 	if not player.gui.left.admin_pane.character then
-		admin_pane.add{name = "character", type = "button", caption = "Character"}
+		admin_pane.add { name = "character", type = "button", caption = "Character" }
 	end
 	if global.player_character_stats[index] == nil then
 		global.player_character_stats[index] = {
@@ -385,12 +391,12 @@ function force_spectators(index, teleport)
 			local pos = player.position
 			if global.player_spectator_character[index] and global.player_spectator_character[index].valid then
 				if not teleport then player.print("Returning you to your character.") end
-				player.set_controller{type = defines.controllers.character, character = global.player_spectator_character[index]}
+				player.set_controller { type = defines.controllers.character, character = global.player_spectator_character[index] }
 			else
 				player.print("Character missing, will create new character at spawn.")
-				player.set_controller{type = defines.controllers.character, character = player.surface.create_entity{name = "player", position = {0, 0}, force = global.player_spectator_force[index]}}
-				player.insert{name = "pistol", count = 1}
-				player.insert{name = "firearm-magazine", count = 10}
+				player.set_controller { type = defines.controllers.character, character = player.surface.create_entity { name = "player", position = { 0, 0 }, force = global.player_spectator_force[index] } }
+				player.insert { name = "pistol", count = 1 }
+				player.insert { name = "firearm-magazine", count = 10 }
 			end
 			if teleport then
 				player.print("Teleporting you to the location you are currently looking at.")
@@ -413,15 +419,15 @@ function force_spectators(index, teleport)
 	else
 		--put player in spectator mode
 		if player.character then
-			player.walking_state = {walking = false, direction = defines.direction.north}
+			player.walking_state = { walking = false, direction = defines.direction.north }
 			global.player_spectator_character[index] = player.character
 			global.player_spectator_force[index] = player.force
-			player.set_controller{type = defines.controllers.god}
+			player.set_controller { type = defines.controllers.god }
 			player.cheat_mode = true
 		end
 		if not game.forces["Admins"] then
 			game.create_force("Admins")
-			for _,f in pairs(game.forces) do
+			for _, f in pairs(game.forces) do
 				f.set_cease_fire("Admins", true)
 				game.forces["Admins"].set_cease_fire(f, true)
 			end
@@ -429,13 +435,13 @@ function force_spectators(index, teleport)
 		player.force = game.forces["Admins"]
 		global.player_spectator_state[index] = true
 		player.print("You are now a spectator")
-		
+
 		-- Creates a Spectator Panel
-		local spectate_panel = player.gui.left.add{name = "spectate_panel", type = "frame", direction = "vertical", caption = "Spectator Mode"}
-		spectate_panel.add{name = "teleport", type = "button", caption = "Teleport"}
-		spectate_panel.add{name = "return_character", type = "button", caption = "Return"}
+		local spectate_panel = player.gui.left.add { name = "spectate_panel", type = "frame", direction = "vertical", caption = "Spectator Mode" }
+		spectate_panel.add { name = "teleport", type = "button", caption = "Teleport" }
+		spectate_panel.add { name = "return_character", type = "button", caption = "Return" }
 		player.gui.left.admin_pane.spectate.caption = "Spectating"
-		
+
 		if player.gui.left.character_panel ~= nil then
 			player.gui.left.character_panel.destroy()
 			player.gui.left.admin_pane.character.caption = "Disabled"
@@ -445,7 +451,7 @@ function force_spectators(index, teleport)
 		end
 		-- adds an option to follow another player.
 		if spectate_panel.follow_panel == nil then
-			spectate_panel.add{name = "follow", type = "button", caption = "Follow"}
+			spectate_panel.add { name = "follow", type = "button", caption = "Follow" }
 		end
 	end
 end
@@ -453,7 +459,7 @@ end
 function admin_reveal(event)
 	if not game.forces["Admins"] then
 		game.create_force("Admins")
-		for _,f in pairs(game.forces) do
+		for _, f in pairs(game.forces) do
 			f.set_cease_fire("Admins", true)
 			game.forces["Admins"].set_cease_fire(f, true)
 		end
