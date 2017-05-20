2636e86f4489e6ce171c962ff35578da12d95bd7
Zachary Sistrunk <computerboyzack@gmail.com>
2017-05-20 01:33:09 +0000

    Update bot.lua

--- bot.lua
+++ bot.lua
@@ -1,27 +1,27 @@
 Event.register(defines.events.on_player_died, function (event)
 	local player = game.players[event.player_index]
-	if player.name ~= nil then
+	if player ~= nil then
 		print("PLAYER$die," .. player.index .. "," .. player.name .. "," .. player.force.name)
 	end
 end)
 
 Event.register(defines.events.on_player_respawned, function (event)
 	local player = game.players[event.player_index]
-	if player.name ~= nil then
+	if player ~= nil then
 		print("PLAYER$respawn," .. player.index .. "," .. player.name .. "," .. player.force.name)
 	end
 end)
 
 Event.register(defines.events.on_player_joined_game, function (event)
 	local player = game.players[event.player_index]
-	if player.name ~= nil then
+	if player ~= nil then
 		print("PLAYER$join," .. player.index .. "," .. player.name .. "," .. player.force.name)
 	end
 end)
 
 Event.register(defines.events.on_player_left_game, function (event)
 	local player = game.players[event.player_index]
-	if player.name ~= nil then
+	if player ~= nil then
 		print("PLAYER$leave," .. player.index .. "," .. player.name .. "," .. player.force.name)
 	end
 end)
@@ -29,7 +29,7 @@ end)
 Event.register(defines.events.on_player_changed_force, function (event)
 	local player = game.players[event.player_index]
 	if (player.force.name == "Admins") then return end
-	if player.name ~= nil then
+	if player ~= nil then
 		print("PLAYER$force," .. player.index .. "," .. player.name .. "," .. player.force.name)
 	end
 end)
